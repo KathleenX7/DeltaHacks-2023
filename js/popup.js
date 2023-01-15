cd0fd8058603a2dbf96074c75610fc5f349dfd5d
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-01-14 21:27:22 -0500

    fixed stuff

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,21 @@
+const FACULTY_MAP = {
+    "art": "#F90",
+    "engineering": "#F0F",
+    "environment": "#0F0",
+    "health": "#0FF",
+    "mathematics": "#F99",
+    "science": "#00F"
+}
+
+const images = document.querySelectorAll(".imageClass");
 
 function getFaculty() {
     chrome.storage.sync.get(["faculty"]).then((result) => {
+        // console.log(document.getElementsByClassName("imageClass"));
+        // console.log(result.faculty);
+        images.forEach(image => {
+            image.style.borderColor = FACULTY_MAP[result.faculty];
+        })
         document.getElementById("idk-some-thingy").innerHTML = result.faculty;
         return result.faculty;
     });
